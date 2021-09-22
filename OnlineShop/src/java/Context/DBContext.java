9a85f2ddb73a0a094c2492a9600fbd6cb4feaa05
nghia <nghianm2803@gmail.com>
2021-09-22 09:46:42 +0700

    FE: Home.jsp + Left.jsp + Menu.jsp
    Chưa ghép api

--- OnlineShop/src/java/Context/DBContext.java
+++ OnlineShop/src/java/Context/DBContext.java
@@ -20,7 +20,7 @@ public class DBContext {
     /*Insert your other code right after this comment*/
     /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
     private final String serverName = "DESKTOP-RQC147G";
-    private final String dbName = "wish1";
+    private final String dbName = "wish1"; // Change db name
     private final String portNumber = "1433";
     private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
